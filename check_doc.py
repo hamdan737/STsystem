e47179d3d4f3aac0767e14b1d3694b788c2c1814
hamdan737 <48652597+hamdan737@users.noreply.github.com>
2021-01-22 01:20:48 +0300

    Create check_doc.py

---
+++ check_doc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import time
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import mysql.connector
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="docadmin",
+  passwd="12345",
+  database="STsystem"
+)
+
+cursor = db.cursor()
+reader = SimpleMFRC522()
+
+try:
+  while True:
+    id, text = reader.read()
+    cursor.execute("Select id,Department_Name,Sublocation,Location FROM dep WHERE RFID="+str(id))
+    result = cursor.fetchone()
+
+    if cursor.rowcount >= 1:
+      cursor.execute("INSERT INTO docs (RFID,Department_ID,Department_Name,Employee,Sublocation,Location) VALUES (%s,%s,%s,%s,%s,%s)", (id,id,result[1],text,result[2],result[3]) )
+      db.commit()
+      time.sleep(2)
+finally:
+  GPIO.cleanup()
