9dcc2a4c13f8090bcee4ffa2e852a68c22c188d9
hamdan737 <48652597+hamdan737@users.noreply.github.com>
2021-01-24 05:02:19 +0300

    Update and rename new_dep.py to new_doc.py

---
+++ new_doc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import time
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import mysql.connector
+
+db = mysql.connector.connect(
+  host="localhost",
+  user="docadmin",
+  passwd="12345",
+  database="stsystem"
+)
+
+cursor = db.cursor()
+reader = SimpleMFRC522()
+
+try:
+  while True:
+    id, text = reader.read()
+    cursor.execute("SELECT * FROM st_users  WHERE user_level="+str(1))
+    results = cursor.fetchone()
+
+    if results[5]==1:
+    sql_insert = "INSERT INTO st_documents  (doc_name,doc_rfid,employee_id,employee_name,sub_location,location) VALUES (%s, %s,%s,%s,%s,%S)"
+    cursor.execute(sql_insert, (text,id,results[0],results[2],results[9],results[8]))
+    else:
+      
+    db.commit()
+
+    time.sleep(2)
+finally:
+    GPIO.cleanup()
